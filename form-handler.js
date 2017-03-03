6c713cd4a6cf14aaf2cb98dd6671bec815ac4070
RyanADesigns <[ryan.stupey@gmail.com]>
2017-03-02 17:48:22 -0800

    fixing git issues

--- form-handler.js
+++ form-handler.js
@@ -1,4 +1,7 @@
-function capture(){
-  console.log(document.custInfo.name.value);
-  event.preventDefault();
+var intiger = prompt("give me a number")
+if (intiger <= 10) {
+  alert("please give me an integer greater than 10")
+} else if (intiger >=20) {
+  alert("please choose a number less than 20 but greater than 10")
 }
+prompt(intiger)
